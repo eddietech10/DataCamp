d214f2309c15faf6a367365c117fdc807e6a456d
Eddie10 <je.bedoya.2019@alumnos.urjc.es>
2022-12-12 18:43:37 +0100

    Start the path

---
+++ Data Analyst in SQL/1. Understanding Data Visualization/1. Visualizing distributions/continuous_vs_categorical_variables.sql
@@ -0,0 +1,9 @@
+/* Continuous vs. categorical variables
+In order to choose an appropriate type of plot to draw, you need to be able to distinguish between continuous variables (roughly: "things you can do arithmetic on") and categorical variables (roughly: "things that can be classified"). 
+
+State which of these variables are continuous and which are categorical.
+*/
+
+-- https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F952bef0d-93c4-46dc-8075-1acd687672c6%2FScreenshot_2022-12-12_at_18.39.25.png?table=block&id=a454f9a6-1f3e-465c-ba2a-318612adfe58&spaceId=5aa1d91e-a06d-4390-b2b1-5a8fec928f89&width=2000&userId=9de1bd4f-e20d-4039-b601-7dad1b970c1f&cache=v2
+
+/* Classy classification! The distinction between continuous variables (numbers) and categorical variables (text) is important when deciding what kind of plot to draw. */
---
+++ Data Analyst in SQL/1. Understanding Data Visualization/1. Visualizing distributions/motivating_visualization.sql
@@ -0,0 +1,11 @@
+/*To get an insight from a dataset, you can calculate summary statistics or run statistical models, but often it's easier to draw a plot.
+
+In this exercise, you can see the price of the Bitcoin cryptocurrency from the start of 2016 to the start of 2020. Columns in the table are filterable and sortable.
+
+Look at the Bitcoin prices on January the first each year. Which year began with the highest Bitcoin price?
+
+Data Source: Coindesk: https://www.coindesk.com/price/bitcoin */
+
+-- https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F99d474f3-1b28-4783-bd07-b0182529bb25%2FScreenshot_2022-12-12_at_18.32.42.png?table=block&id=abab262e-d74f-4eba-abf7-722881285468&spaceId=5aa1d91e-a06d-4390-b2b1-5a8fec928f89&width=2000&userId=9de1bd4f-e20d-4039-b601-7dad1b970c1f&cache=v2
+
+/* Perfect price plot probing! Often, looking at a plot is a quicker way to answer questions than poring through data. */
