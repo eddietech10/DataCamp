5efb2bc56bd9718a3a4825d09037f3de5db74547
Eddie10 <je.bedoya.2019@alumnos.urjc.es>
2022-12-12 23:07:31 +0100

    First milestone

---
+++ Data Analyst in SQL/1. Understanding Data Visualization/1. Visualizing distributions/interpreting_histograms.sql
@@ -0,0 +1,7 @@
+/* Here is a histogram of salaries for various jobs in Australia. Each row of the dataset is the average salary for that job, so the counts are counts of jobs.
+
+*Tip: This left-hand pane of the exercise containing text and instructions is resizable. If the plot is too small to see clearly, making the pane wider will increase the plot size. Move your mouse in-between the left-hand pane and the drag and drop portion of the exercise so a gray vertical bar appears. In Chrome, click and drag this bar to the right. In Firefox, click the bar, move your mouse right, then click again.*
+
+Data Source: **[Tidy Tuesday](https://github.com/rfordatascience/tidytuesday/blob/master/data/2018/2018-04-23/week4_australian_salary.csv)** 
+
+Categorize these statements about the histogram as true or false.*/
